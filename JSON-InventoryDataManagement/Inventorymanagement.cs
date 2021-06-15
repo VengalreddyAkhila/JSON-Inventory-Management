d75bfe6ad3419dc1d9c3653f4b5bbb42ce4a8da8
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-15 22:22:27 +0530

    Added:Inventories

---
+++ JSON-InventoryDataManagement/Inventorymanagement.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JSON_InventoryDataManagement
+{
+    class Inventorymanagement
+    {
+        /// <summary>
+        /// intializing the inventories by using json file
+        /// </summary>
+
+        public void GetData()
+        {
+            try
+            {
+                var json = File.ReadAllText(@"C:\Users\divak\Source\Repos\JSON-InventoryDataManagement\JSON-InventoryDataManagement\json1.json");
+                var jobject = JObject.Parse(json);
+
+                JArray inventoriesArr = (JArray)jobject["inventories"];
+                if (inventoriesArr != null)
+                {
+                    foreach (var item in inventoriesArr)
+                    {
+                        Console.WriteLine("name is = " + item["name"]);
+                        Console.WriteLine("price is = " + item["price"]);
+                        Console.WriteLine("weight is = " + item["weight"]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error is :" + ex.Message.ToString());
+            }
+        }
+        /// <summary>
+        /// calculating the inventories
+        /// using jtoken calculating waight and price
+        /// </summary>
+        /// <param name="weight"></param>
+        /// <param name="price"></param>
+
+
+        public void GetValue(JToken weight, JToken price)
+        {
+            int weightConv = weight.ToObject<int>();
+            int priceConv = price.ToObject<int>();
+            try
+            {
+                int totalValue = weightConv * priceConv;
+                Console.WriteLine($"Value of Inventories : {totalValue}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
+            
+            
+            
+
+
+
+
+
+               
+                
+    
+
---
+++ JSON-InventoryDataManagement/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JSON_InventoryDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to JSON Inventorydatamanagement");
+            Inventorymanagement inventory = new Inventorymanagement();
+            inventory.GetData();
+            inventory.GetValue(35,10);
+                
+            
+           
+        }
+    }
+}
